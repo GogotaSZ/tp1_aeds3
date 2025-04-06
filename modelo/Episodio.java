de02d8393ab34191ad16f417adeadb6c7327ca01
GogotaSZ <gilhermeAS1@gmail.com>
2025-04-05 22:51:02 -0300

    add Ep and visionEp .java

---
+++ modelo/Episodio.java
@@ -0,0 +1,106 @@
+import java.io.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import aed3.Registro;
+
+public class Episodio implements Registro {
+
+    private int id;
+    private int idSerie;
+    private String nome;
+    private int temporada;
+    private LocalDate dataLancamento;
+    private int duracao;
+    private String sinopse;
+
+    public Episodio() {
+        this.id = -1;
+        this.idSerie = -1;
+        this.nome = "";
+        this.temporada = 0;
+        this.dataLancamento = LocalDate.now();
+        this.duracao = 0;
+        this.sinopse = "";
+    }
+
+    public Episodio(int idSerie, String nome, int temporada, LocalDate dataLancamento, int duracao, String sinopse) {
+        this.id = -1;
+        this.idSerie = idSerie;
+        this.nome = nome;
+        this.temporada = temporada;
+        this.dataLancamento = dataLancamento;
+        this.duracao = duracao;
+        this.sinopse = sinopse;
+    }
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdSerie() {
+        return idSerie;
+    }
+
+    public void setIdSerie(int idSerie) {
+        this.idSerie = idSerie;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public LocalDate getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    // Serialização
+    public byte[] toByteArray() throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(baos);
+
+        dos.writeInt(id);
+        dos.writeInt(idSerie);
+        dos.writeUTF(nome);
+        dos.writeInt(temporada);
+        dos.writeLong(dataLancamento.toEpochDay());
+        dos.writeInt(duracao);
+        dos.writeUTF(sinopse);
+
+        return baos.toByteArray();
+    }
+
+    // Desserialização
+    public void fromByteArray(byte[] b) throws IOException {
+        ByteArrayInputStream bais = new ByteArrayInputStream(b);
+        DataInputStream dis = new DataInputStream(bais);
+
+        id = dis.readInt();
+        idSerie = dis.readInt();
+        nome = dis.readUTF();
+        temporada = dis.readInt();
+        dataLancamento = LocalDate.ofEpochDay(dis.readLong());
+        duracao = dis.readInt();
+        sinopse = dis.readUTF();
+    }
+
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return "\nID.............: " + id +
+                "\nID da Série....: " + idSerie +
+                "\nNome...........: " + nome +
+                "\nTemporada......: " + temporada +
+                "\nData Lançamento: " + dataLancamento.format(formatter) +
+                "\nDuração........: " + duracao + " min" +
+                "\nSinopse........: " + sinopse;
+    }
+}
---
+++ visao/VisaoEpisodios.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+public class VisaoEpisodios {
+
+    private Scanner sc = new Scanner(System.in);
+
+    public Episodio leEpisodio(int idSerie) {
+        try {
+            System.out.print("Nome do episódio: ");
+            String nome = sc.nextLine();
+
+            System.out.print("Temporada: ");
+            int temporada = Integer.parseInt(sc.nextLine());
+
+            LocalDate dataLancamento = null;
+            boolean dataValida = false;
+            while (!dataValida) {
+                System.out.print("Data de lançamento (AAAA-MM-DD): ");
+                String dataStr = sc.nextLine();
+                try {
+                    dataLancamento = LocalDate.parse(dataStr); // formato ISO
+                    dataValida = true;
+                } catch (DateTimeParseException e) {
+                    System.out.println("Data inválida! Tente novamente.");
+                }
+            }
+
+            System.out.print("Duração (minutos): ");
+            int duracao = Integer.parseInt(sc.nextLine());
+
+            System.out.print("Sinopse: ");
+            String sinopse = sc.nextLine();
+
+            return new Episodio(idSerie, nome, temporada, dataLancamento, duracao, sinopse);
+
+        } catch (Exception e) {
+            System.out.println("Erro ao ler episódio: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public void mostraEpisodio(Episodio e) {
+        if (e != null) {
+            System.out.println("\n--- Detalhes do Episódio ---");
+            System.out.println(e.toString());
+            System.out.println("----------------------------\n");
+        } else {
+            System.out.println("Episódio inválido.");
+        }
+    }
+}
