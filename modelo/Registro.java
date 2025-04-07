431622153a3adf716077bc78a3240e067f81f6bc
Daniel <danivitorcosta@gmail.com>
2025-04-06 23:44:58 -0300

    reorganiza classes Registro e adicao classe generica hash extensível
    - Move Registro.java de modelo/ para util/
    - Implementa classes para hash extensível:
      * HashExtensivel.java (nova)
      * ParIDEndereco.java (nova)
      * RegistroHashExten.java (nova)
    - Modifica Arquivo.java para suportar nova estrutura
    - Atualiza Serie.java que chama Registro.util

--- modelo/Registro.java
+++
@@ -1,8 +0,0 @@
-package modelo;
-
-public interface Registro {
-    int getId();
-    void setId(int id);
-    byte[] toByteArray() throws Exception;
-    void fromByteArray(byte[] ba) throws Exception;
-}
